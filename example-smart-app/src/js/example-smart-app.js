be09f8e0989e06c0dc0800a22cc81a65059c8e2c
michaelcafiero99 <78000917+michaelcafiero99@users.noreply.github.com>
2022-07-14 13:11:20 -0500

    condition`

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,22 +15,23 @@
         console.log("has patient")
         var patient = smart.patient;
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
 
         var conds = smart.patient.api.fetchAll({
           type: 'Condition',
           query:{_count: 4}
 
         });
+        var obv = smart.patient.api.fetchAll({
+          type: 'Observation',
+          query: {
+            code: {
+              $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+            }
+          }
+        });
+
 
         $.when(pt, conds).done(function(patient, conds) {
           console.log(conds);
@@ -50,7 +51,6 @@
           console.log(byCodes)
           console.log(byCodesCond('75323-6'))
           var gender = patient.gender;
-          console.log(cond)
 
           var fname = '';
           var lname = '';
