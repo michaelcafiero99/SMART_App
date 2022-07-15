90d066bb7d0db46ea495273c9ceb40b2ad570ecb
michaelcafiero99 <78000917+michaelcafiero99@users.noreply.github.com>
2022-07-15 12:49:41 -0500

    condition`

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -50,7 +50,7 @@
           var byCodesCond = smart.byCodes(conds, 'code');
           console.log(byCodes)
           console.log(byCodesCond('75323-6'))
-          var gender = patient.address;
+          var gender = patient.address[0];
 
           var fname = '';
           var lname = '';
